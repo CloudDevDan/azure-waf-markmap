8467e4265e741b1ed65432d2583aa5ba4616e64e
CloudDevDan <daniel_mcloughlin@outlook.com>
2025-06-24 14:18:27 +0100

    new favicon added

--- postprocess-map.js
+++ postprocess-map.js
@@ -9,15 +9,16 @@ try {
   process.exit(1);
 }
 
-// // ✅ Inject favicon and manifest links if missing
-// if (!html.includes('apple-touch-icon')) {
-//   html = html.replace('</head>', `<link rel="icon" type="image/png" href="favicon/favicon.png">
-// <link rel="apple-touch-icon" sizes="180x180" href="favicon/apple-touch-icon.png">
-// <link rel="icon" type="image/png" sizes="32x32" href="favicon/favicon-32x32.png">
-// <link rel="icon" type="image/png" sizes="16x16" href="favicon/favicon-16x16.png">
-// <link rel="manifest" href="favicon/site.webmanifest">
-// </head>`);
-// }
+// ✅ Inject favicon and manifest links if missing
+if (!html.includes('apple-touch-icon')) {
+  html = html.replace('</head>', `<link rel="icon" type="image/png" href="favicon/favicon.png">
+<link rel="apple-touch-icon" sizes="180x180" href="favicon/apple-touch-icon.png">
+<link rel="icon" type="image/png" sizes="32x32" href="favicon/favicon-32x32.png">
+<link rel="icon" type="image/png" sizes="16x16" href="favicon/favicon-16x16.png">
+<link rel="manifest" href="favicon/site.webmanifest">
+</head>`);
+}
+// https://favicon.io/emoji-favicons/world-map
 
 // ✅ Update <title>
 html = html.replace(
